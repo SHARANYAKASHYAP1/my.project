e3525cdc4ad7b39a322c1b15846d045554701a9e
SHARANYAKASHYAP1 <100034830+SHARANYAKASHYAP1@users.noreply.github.com>
2023-06-09 18:21:36 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,23 @@
+var y = Math.floor(Math.random() * 10 + 1);
+var x = document.getElementById("guessField")
+
+if(x == y)
+{
+    alert("CONGRATULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN "
+       + guess + " GUESS");
+       guess= 1;
+}
+else if(x > y) 
+{
+    guess++;
+    alert("OOPS SORRY !! TRY A SMALLER NUMBER");
+}
+else
+{
+  guess++;  
+    alert("OOPS SORRY !! TRY A GRAETER NUMBER");
+}
+function playAgain(){
+ y = Math.floor(Math.random() * 10 + 1);
+}
+
